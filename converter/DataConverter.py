5ef9c5c6f855dcad6f5578022535f7e77bd30e5e
Cindy Leschaud <cindy.leschaud@liip.ch>
2024-12-17 10:56:52 +0100

    Search and replace ß

--- converter/DataConverter.py
+++ converter/DataConverter.py
@@ -1,3 +1,6 @@
+from model.StructuredData import SimplificationResponse
+
+
 class DataConverter:
 
     def __init__(self, payload, simplifier, model: str = None) -> None:
@@ -10,4 +13,17 @@ class DataConverter:
         self.leichte_sprache = bool(payload.leichte_sprache)
 
     def simplify(self):
-        return self.simplifier.simplify_text(self.json, self.leichte_sprache)
+        results = self.simplifier.simplify_text(self.json, self.leichte_sprache)
+
+        # Parse the JSON string into a Python dictionary
+        data = results.dict()
+
+        # Iterate over each item in the "simplifications" list
+        for item in data["simplifications"]:
+            # Replace ß with ss in the "text" key
+            item["text"] = item["text"].replace("ß", "ss")
+
+        # Convert the updated dictionary back to a JSON string
+        simplifications = SimplificationResponse.parse_obj(data)
+
+        return simplifications
